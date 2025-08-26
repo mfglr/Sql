ef989e57983425a032b85ade9be08a4a0ad04f47
mfglr <mfgglr@outlook.com>
2025-08-26 22:48:44 +0300

    odev10

---
+++ odev10.sql
@@ -0,0 +1,22 @@
+SELECT 
+    city.city AS city_name,
+    country.country AS country_name
+FROM city
+LEFT JOIN country
+    ON city.country_id = country.country_id;
+
+SELECT 
+    payment.payment_id,
+    customer.first_name,
+    customer.last_name
+FROM payment
+RIGHT JOIN customer
+    ON payment.customer_id = customer.customer_id;
+
+SELECT
+    rental.rental_id,
+    customer.first_name,
+    customer.last_name
+FROM rental
+FULL JOIN customer
+    ON rental.customer_id = customer.customer_id;
