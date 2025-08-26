25142b02ba699b9b3f8d71c6da1df751a305b17a
mfglr <mfgglr@outlook.com>
2025-08-26 22:58:57 +0300

    odev11

---
+++ odev11.sql
@@ -0,0 +1,18 @@
+SELECT first_name FROM actor
+UNION
+SELECT first_name FROM customer
+ORDER BY first_name;
+
+SELECT first_name
+FROM actor
+INTERSECT
+SELECT first_name
+FROM customer
+ORDER BY first_name;
+
+SELECT first_name
+FROM actor
+EXCEPT
+SELECT first_name
+FROM customer
+ORDER BY first_name;
